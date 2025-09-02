7fe57d38d0207ffb7cdc465103f416390f533057
ibb <ibb@develup.pro>
2025-09-02 16:44:38 +0300

    Setup
    - eslint rule fix
    - local fonts removed
    - sprites gitignore added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
-    'vue/html-self-closing': false,
+    "vue/html-self-closing": ["error", false],
   },
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// import { getFontsPreloadList } from './app/utils/helpers';
-
-// const fontsToPreload = getFontsPreloadList([
-//   {
-//     path: 'Inter/Inter-',
-//     weights: ['Light', 'Regular', 'Medium', 'SemiBold', 'Bold'],
-//   },
-// ]);
-
 export default defineNuxtConfig({
   app: {
     head: {
       link: [
         // fonts
-
-        // {
-        //   rel: 'preconnect',
-        //   href: 'https://fonts.googleapis.com',
-        // },
-        // {
-        //   rel: 'preconnect',
-        //   href: 'https://fonts.gstatic.com',
-        //   crossorigin: 'anonymous'
-        // },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
         {
           rel: 'preload',
           as: 'style',
           onload: "this.rel='stylesheet'",
           href: 'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300..700&display=swap',
         },
-
-        // ...fontsToPreload,
       ],
     },
   },
@@ -48,14 +36,4 @@ export default defineNuxtConfig({
       },
     },
   },
-
-  // Nuxt image
-  // image: {
-  // опціонально:
-  // dir: 'public', // де лежать ваші статичні зображення
-  // screens: { sm: 640, md: 768, lg: 1024, xl: 1280 }, // брейкпоінти для sizes
-  // provider: 'ipx', // дефолтний transformer
-  // domains: ['example.com'], // якщо тягнете зовнішні картинки
-  // presets: { avatar: { modifiers: { width: 128, height: 128, fit: 'cover' } } },
-  // },
 });
